dc23ba95f16d3c2f58ab01df600e06cdf24c1850
Aadityavv <aadityavv9@gmail.com>
2024-06-04 00:42:20 +0530

    new files added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CAROUSEL
 document.addEventListener('DOMContentLoaded', () => {
     const slidesData = [
       { src: './images/industrialWarehouse.jpeg', description: 'Industrial Warehouse/ Factory' },
